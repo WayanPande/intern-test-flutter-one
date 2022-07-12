df8f6ea2bc6e8afe5f79fee332c5f6130e91f5af
WayanPande <yande554@gmail.com>
2022-07-13 07:23:25 +0800

    initial commit

---
+++ lib/screens/first_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Text(
+          "Test",
+        ),
+      ),
+    );
+  }
+}
